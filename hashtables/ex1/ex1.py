6ad119c27b08d65fe231b0e5b63cee3863d6d637
jawad727 <jawaddhussein@gmail.com>
2019-07-12 11:23:58 -0700

    MVP Complete, all tests passing for hash Qs

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,18 +6,29 @@ from hashtables import (HashTable,
                         hash_table_resize)
 
 
+
+
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    # Adding all weights to hash table
+    for index in range(0, length):
+        hash_table_insert(ht, weights[index], index)
+
+    # Check if limit - weights[index] is True, if so we found the solution
+    for index in range(0, length):
+        if hash_table_retrieve(ht, limit - weights[index]):
+            # If one index is greater then the other put it first in the tuple
+            if index > hash_table_retrieve(ht, limit - weights[index]):
+                return (index, hash_table_retrieve(ht, limit - weights[index]))
+            else:
+                return (hash_table_retrieve(ht, limit - weights[index]), index)
 
     return None
 
 
 def print_answer(answer):
-    if answer is None:
+    if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -12,12 +12,22 @@ class Ticket:
         self.destination = destination
 
 
+
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = ["NONE"] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # Looping through each ticket, adding it to route if it has a source of "NONE"
+    # If not add it to the hash table
+    for t in tickets:
+        if t.source == "NONE":
+            route[0] = t.destination
+        else:
+            hash_table_insert(hashtable, t.source, t.destination)
+
+    # looping through route to populate
+    for i in range(1, len(tickets)):
+        # next available route will equal the destination from the previous one
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
 
     return route
